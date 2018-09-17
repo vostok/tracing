86b265facd0a80593a33a87d0aa7e6bac3ed884f
iloktionov <iloktionov@skbkontur.ru>
2018-09-17 18:45:26 +0500

    Added a stub for annotations inheritance extension.

---
+++ Vostok.Tracing/WithInheritanceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing
+{
+    [PublicAPI]
+    public static class WithInheritanceExtensions
+    {
+        [Pure]
+        public static ITracer WithAnnotationsInheritance(
+            [NotNull] this ITracer tracer,
+            [NotNull] string[] names,
+            bool allowOverwrite = false,
+            int inheritanceDepth = 1)
+        {
+            // TODO(iloktionov): implement
+
+            throw new NotImplementedException();
+        }
+    }
+}
